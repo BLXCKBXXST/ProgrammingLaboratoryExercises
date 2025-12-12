2324eddbbf2f9ca82eed1bdf7f3f0875837855d0
blxckweed <blxckbusta@bk.ru>
2025-12-12 10:42:49 +0700

    Added sort.c -- bubble sort function

--- sort.c
+++ sort.c
@@ -1,13 +1,10 @@
-// Bubble Sort
-#include <stdio.h>
-
-void bubble_sort(int a[], int n) {
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - 1 - i; j++) {
-            if (a[j] > a[j + 1]) {
-                int tmp = a[j];
-                a[j] = a[j + 1];
-                a[j + 1] = tmp;
+void bubble_sort(int arr[], int n) {
+    for (int i = 0; i < n-1; i++) {
+        for (int j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
             }
         }
     }
